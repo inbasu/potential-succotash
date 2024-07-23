c982fc79b4ed2b877c0f8a8ceca1e2f4593eba9d
Inbasu <just.me.juncy@gmail.com>
2024-07-24 02:14:20 +0300

    new file:   lua/core/plugin_config/nvim-cmp.lua

---
+++ lua/core/plugin_config/nvim-cmp.lua
@@ -0,0 +1,37 @@
+local cmp = require("cmp")
+
+
+cmp.setup {
+        
+        window = {},
+        snippet = {
+                expand = function(args)
+                require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+        },
+        
+        mapping = cmp.mapping.preset.insert({
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                                cmp.select_next_item()
+                        else
+                                fallback()
+                        end
+                end, { "i", "s" }), 
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                                cmp.select_prev_item()
+                        else
+                                fallback()
+                        end
+                end, { "i", "s" }),
+        }),
+
+        sources = cmp.config.sources({
+                { name = 'nvim_lsp' },
+                { name = 'luasnip' },
+                { name = 'buffer' }, 
+        })
+}
