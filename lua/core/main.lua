e77c4665b81562b9b04d0f2f75d8c7ad97c53b3c
Inbasu <just.me.juncy@gmail.com>
2024-07-30 22:44:01 +0300

    modified:   main.lua

--- lua/core/main.lua
+++ lua/core/main.lua
@@ -11,6 +11,9 @@ vim.opt.expandtab = true
 -- keymap
 vim.g.mapleader = " "
 vim.keymap.set("n",   "<leader>pv", vim.cmd.Ex)
+-- remap Caps Lock
+vim.api.nvim_create_autocmd("VimEnter", {command = "silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'",})
+vim.api.nvim_create_autocmd("VimLeave", {command = "!xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'",})
 -- color
 function ColorVim(color)
         color = color or "material"
