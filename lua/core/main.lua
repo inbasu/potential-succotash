2c17365cb765e182f622bc1997eb61ef7f7e5209
Inbasu <just.me.juncy@gmail.com>
2024-09-18 00:21:18 +0300

    modified:   lua/core/main.lua

--- lua/core/main.lua
+++ lua/core/main.lua
@@ -10,7 +10,7 @@ vim.opt.expandtab = true
 
 -- keymap
 vim.g.mapleader = " "
-vim.keymap.set("n",   "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- remap Caps Lock
 vim.api.nvim_create_autocmd("VimEnter", {command = "silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'",})
 vim.api.nvim_create_autocmd("VimLeave", {command = "!xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'",})
